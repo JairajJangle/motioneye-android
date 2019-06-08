8524f01041f4282afa0920332966246e48282bcb
JairajJangle <jairaj.jangle@gmail.com>
2019-06-08 19:22:09 +0530

    Reduced App Size + Added alert when web page fails to load.

--- app/src/main/java/com/jairaj/janglegmail/motioneye/web_motion_eye.java
+++ app/src/main/java/com/jairaj/janglegmail/motioneye/web_motion_eye.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -22,38 +23,20 @@ import android.view.Window;
 import android.webkit.CookieManager;
 import android.webkit.DownloadListener;
 import android.webkit.URLUtil;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import java.util.Objects;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class web_motion_eye extends AppCompatActivity //implements SwipeRefreshLayout.OnRefreshListener
 {
     private static final String TAG = "";
     private ProgressDialog progressBar;
     private AlertDialog cancel_button;
 
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     Handler mHandler = new Handler();
@@ -74,10 +57,10 @@ public class web_motion_eye extends AppCompatActivity //implements SwipeRefreshL
             mContentView.setSystemUiVisibility(
                     //View.SYSTEM_UI_FLAG_LOW_PROFILE
                     View.SYSTEM_UI_FLAG_FULLSCREEN
-                    //|View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            //|View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             );
         }
     };
@@ -224,6 +207,46 @@ public class web_motion_eye extends AppCompatActivity //implements SwipeRefreshL
 
 //                swipe.setRefreshing(false);
             }
+
+            public void onReceivedError(WebView view, WebResourceRequest request,
+                                        WebResourceError error)
+            {
+                super.onReceivedError(view, request, error);
+
+                //TODO: remove this debug toast
+                Toast.makeText(getApplicationContext(), "Error" + error.toString(),
+                        Toast.LENGTH_LONG).show();
+
+                new AlertDialog.Builder(web_motion_eye.this)
+                        .setTitle("Uh Oh! The camera stream could not be opened :( ")
+                        .setMessage("Please help us fix the issue by letting us know by sending a feedback")
+
+                        .setPositiveButton("Send Feedback", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                sendFeedback();
+                            }
+
+                        })
+
+                        .setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                            }
+                        })
+                        .setNegativeButton("Check Help and FAQ", new DialogInterface.OnClickListener()
+                        {
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                Intent i = new Intent(web_motion_eye.this, Help_FAQ.class);
+                                finish();  //Kill the activity from which you will go to next activity
+                                startActivity(i);
+                            }
+                        })
+                        .setCancelable(false)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
         });
         mContentView.loadUrl(url_port);
 
@@ -319,10 +342,11 @@ public class web_motion_eye extends AppCompatActivity //implements SwipeRefreshL
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }*/
 
-    /**
+    /*
      * Schedules a call to hide() in delay milliseconds, canceling any
      * previously scheduled calls.
      */
+
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
@@ -386,4 +410,14 @@ public class web_motion_eye extends AppCompatActivity //implements SwipeRefreshL
         super.onSaveInstanceState(outState);
         mContentView.saveState(outState);
     }*/
+
+    private void sendFeedback()
+    {
+        final Intent _Intent_sendFeedback = new Intent(Intent.ACTION_SEND);
+        _Intent_sendFeedback.setType("message/rfc822");
+        _Intent_sendFeedback.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"systems.sentinel@gmail.com"});
+        _Intent_sendFeedback.putExtra(android.content.Intent.EXTRA_SUBJECT, "motionEye app issue report");
+        _Intent_sendFeedback.putExtra(android.content.Intent.EXTRA_TEXT, "Unable to load camera stream");
+        startActivity(Intent.createChooser(_Intent_sendFeedback, "Feedback"));
+    }
 }
